c296b67dfec1fc6847b5ab40c4d9665ef27ac466
Steve Landey <steve@steveasleep.com>
2021-03-06 17:38:14 -0800

    Updates

--- rl21.77de5100.js
+++ rl21.77de5100.js
@@ -51767,7 +51767,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64404" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60403" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -51943,4 +51943,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.ts"], null)
-//# sourceMappingURL=/rl21.77de5100.js.map
+//# sourceMappingURL=rl21.77de5100.js.map
