bce3a616031c60177489e626172c4fa66eab237e
Steve Landey <steve@steveasleep.com>
2021-03-09 16:06:12 -0800

    Fix enemy bug

--- src/ecs/moves/CreateAndFollowGoal.ts
+++ src/ecs/moves/CreateAndFollowGoal.ts
@@ -15,20 +15,20 @@ export class CreateAndFollowGoal implements Move {
   private populateGoal(ctx: MoveContext) {
     const combatC = ctx.entity.getComponent(CombatC);
     // nullify goal if finished
-    if (combatC.goal) {
-      if (
-        combatC.goal.type === GoalType.HuntPlayer &&
-        (combatC.goal.path.length < 1 ||
-          !isAdjacent(
-            combatC.goal.path[combatC.goal.path.length - 1],
-            ctx.ecs.player.getComponent(SpriteC).pos
-          ))
-      ) {
-        console.log("Nullifying goal on", ctx.entity);
-        combatC.goal = null;
-      }
-    }
-    if (combatC.goal) return;
+    // if (combatC.goal) {
+    //   if (
+    //     combatC.goal.type === GoalType.HuntPlayer &&
+    //     (combatC.goal.path.length < 1 ||
+    //       !isAdjacent(
+    //         combatC.goal.path[combatC.goal.path.length - 1],
+    //         ctx.ecs.player.getComponent(SpriteC).pos
+    //       ))
+    //   ) {
+    //     console.log("Nullifying goal on", ctx.entity);
+    //     combatC.goal = null;
+    //   }
+    // }
+    // if (combatC.goal) return;
     combatC.goal = {
       type: GoalType.HuntPlayer,
       playerPos: ctx.ecs.player.getComponent(SpriteC).pos,
@@ -38,11 +38,13 @@ export class CreateAndFollowGoal implements Move {
         ctx.ecs
       ),
     };
+    if (combatC.goal.path.length < 1) {
+      // couldn't find a path
+      combatC.goal = null;
+    }
   }
 
   private getNextMoveInGoal(ctx: MoveContext, consume: boolean): Move | null {
-    this.populateGoal(ctx);
-
     const combatC = ctx.entity.getComponent(CombatC);
     if (combatC.goal) {
       switch (combatC.goal.type) {
@@ -72,6 +74,7 @@ export class CreateAndFollowGoal implements Move {
         message: "Can only execute plan while doing nothing else",
       };
     }
+    this.populateGoal(ctx);
     const nextMove = this.getNextMoveInGoal(ctx, false);
     if (nextMove) {
       const nextMoveResult = nextMove.check(ctx, target);
@@ -86,6 +89,7 @@ export class CreateAndFollowGoal implements Move {
   }
 
   apply(ctx: MoveContext, target: AbstractVector, doNext: () => void): boolean {
+    this.populateGoal(ctx);
     const nextMove = this.getNextMoveInGoal(ctx, true);
     console.log("Executing next move in plan:", nextMove);
     if (nextMove) {
